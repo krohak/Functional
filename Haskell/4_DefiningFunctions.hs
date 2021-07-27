dc149e54a59dbf214bd224db5c05a9a23d7af9d3
krohak <rohaksinghal14@gmail.com>
2021-07-27 18:11:49 +0800

    fun exercises

--- Haskell/4_DefiningFunctions.hs
+++ Haskell/4_DefiningFunctions.hs
@@ -114,7 +114,7 @@ halve xs = splitAt n xs
 thirdA xs = head (tail (tail xs))
 thirdB xs = xs !! 2
 thirdC (_:_:x:_) = x
-thirdC _ = -1
+
 
 --  Consider a function safetail :: [a] -> [a] that behaves in the same way as tail except 
 --  that it maps the empty list to itself rather than producing an error. Using tail and the 
@@ -136,6 +136,30 @@ False || False = False
 _ || _ = True
 
 b || c
-  | not b Prelude.&& b==c = False 
+  | b==c = b
   | otherwise = True
 
+-- The Luhn algorithm is used to check bank card numbers for simple errors such as mistyping a digit, 
+-- and proceeds as follows:
+-- ‚ consider each digit as a separate number;
+-- ‚ moving left, double every other number from the second last;
+-- ‚ subtract 9 from each number that is now greater than 9;
+-- ‚ add all the resulting numbers together;
+-- ‚ if the total is divisible by 10, the card number is valid.
+-- Define a function luhnDouble :: Int -> Int that doubles a digit and subtracts 9 if the result is 
+-- greater than 9. For example:
+-- > luhnDouble 3 
+-- 6
+-- > luhnDouble 6 
+-- 3
+
+luhnDouble x = if (* 2) x > 9 then (* 2) x -9 else  (* 2) x
+
+-- Using luhnDouble and the integer remainder function mod, define a function luhn :: Int -> Int -> Int -> Int -> Bool 
+-- that decides if a four-digit bank card number is valid. For example:
+-- > luhn 1 7 8 4 
+-- True
+-- > luhn 4 7 8 3 
+-- False
+ 
+luhn _ x y z = sum (map luhnDouble [x,y,z]) `mod` 10 == 0
