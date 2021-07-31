860fff7de12c7b299f5b7ea541ee10e2dee18cd7
krohak <rohaksinghal14@gmail.com>
2021-07-31 23:23:52 +0800

    ex

--- Haskell/7_HigherOrderFunctions.hs
+++ Haskell/7_HigherOrderFunctions.hs
@@ -181,7 +181,7 @@ decode = map (chr . bin2int) . chop8
 transmit = decode . channel . encode
 channel = id
 
-t = transmit "higher-order functions are easy" 
+t = transmit "higher-order functions are easy"
 -- "higher-order functions are easy"
 
 
@@ -190,7 +190,7 @@ t = transmit "higher-order functions are easy"
 votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
 
 count x = length . filter (== x)
-c = count "Red" votes 
+c = count "Red" votes
 -- 2
 
 rmdups [] = []
@@ -205,7 +205,7 @@ result vs = sort [(count v vs, v) | v <- rmdups vs]
 winner :: Ord a => [a] -> a
 winner = snd . last . result
 
-b = winner votes 
+b = winner votes
 -- "Blue"
 
 
@@ -213,8 +213,8 @@ b = winner votes
 
 ballots = [["Red", "Green"],
             ["Blue"],
-            ["Green", "Red", "Blue"], 
-            ["Blue", "Green", "Red"], 
+            ["Green", "Red", "Blue"],
+            ["Blue", "Green", "Red"],
             ["Green"]
     ]
 
@@ -239,6 +239,95 @@ winner' bs = case rank (rmempty bs) of
             [c] -> c
             (c:cs) -> winner' (elim c bs)
 
-w1 = winner' ballots 
+w1 = winner' ballots
 -- "Green"
 
+
+{-
+Exercises
+-}
+
+
+-- Show how the list comprehension [f x | x <- xs, p x] can be re-expressed using the higher-order functions map and filter.
+
+mapFilterComp :: (t -> a) -> (t -> Bool) -> [t] -> [a]
+mapFilterComp f p xs = [f x | x <- xs, p x]
+mapFilter :: (t -> a) -> (t -> Bool) -> [t] -> [a]
+mapFilter f p = map f . filter p
+
+-- 2. Without looking at the definitions from the standard prelude, define the following higher-order library functions on lists.
+-- a. Decide if all elements of a list satisfy a predicate:
+-- all2 :: (a -> Bool) -> [Bool] -> Bool
+all2 f = foldr (\x xs -> f x && xs) True
+all3 p = and . map p
+
+-- b. Decide if any element of a list satisfies a predicate:
+-- any :: (a -> Bool) -> [Bool] -> Bool
+any2 f = foldr (\x xs -> f x || xs) False
+any3 p = or . map p
+
+-- c. Select elements from a list while they satisfy a predicate:
+-- takeWhile :: (a -> Bool) -> [a] -> [a]
+
+takeWhile1 f [] = []
+takeWhile1 f (x:xs)
+    | f x = x: takeWhile1 f xs
+    | otherwise = []
+
+-- d. Remove elements from a list while they satisfy a predicate:
+-- dropWhile :: (a -> Bool) -> [a] -> [a]
+dropWhile1 f [] = []
+dropWhile1 f (x:xs)
+    | f x = dropWhile1 f xs
+    | otherwise = x:xs
+
+-- Note: in the prelude the first two of these functions are generic functions rather than being specific to the type of lists.
+
+
+-- Redefine the functions map f and filter p using foldr.
+
+map3 f = foldr (\x xs -> f x: xs) []
+
+filter3 p = foldr (\x xs -> ([x | p x]) ++ xs) []
+
+
+-- Using foldl, define a function dec2int :: [Int] -> Int that converts a
+-- decimal number into an integer. For example:
+-- dec2int [2,3,4,5] 
+-- 2345
+
+dec2int :: [Int] -> Int
+dec2int = foldl (\xs x -> (*10)xs + x) 0
+
+
+-- 5. Without looking at the definitions from the standard prelude, define the higher-order library function curry that converts a function 
+-- on pairs into a curried function, and, conversely, the function uncurry that converts a curried function with two arguments into a function on pairs.
+
+-- curry1 :: ((a0, b0) -> c0) -> a0 -> b0 -> c0
+-- \x.\y = x+y
+-- \ x y = x+y
+
+curry1 :: ((a0, b0) -> c0) -> a0 -> b0 -> c0
+curry1 f x y = f (x,y)
+uncurry1 :: (a0 -> b0 -> c0) -> (a0, b0) -> c0
+uncurry1 f(x,y) = f x y
+
+-- A higher-order function unfold that encapsulates a simple pattern of recursion for producing a list can be defined as follows:
+unfold p h t x 
+    | p x = []
+    | otherwise = h x : unfold p h t (t x)
+-- That is, the function unfold p h t produces the empty list if the predicate p is true of the argument value, and otherwise produces a non-empty list by applying 
+-- the function h to this value to give the head, and the function t to generate another argument that is recursively processed in the same way to produce the tail 
+-- of the list. For example, the function int2bin can be rewritten more compactly using unfold as follows:
+int2bin1 = unfold (== 0) (`mod` 2) (`div` 2)
+-- Redefine the functions chop8, map f and iterate f using unfold.
+chop8two = unfold null (take 8) (drop 8)
+map4 f = unfold null (f . head) tail
+iterate2 f = unfold null f f
+
+
+-- Modify the binary string transmitter example to detect simple transmission errors using the concept of parity bits. That is, each eight-bit binary number produced during 
+-- encoding is extended with a parity bit, set to one if the number contains an odd number of ones, and to zero otherwise. In turn, each resulting nine-bit binary number 
+-- consumed during decoding is checked to ensure that its parity bit is correct, with the parity bit being discarded if this is the case, and a parity error being reported 
+-- otherwise. Hint: the library function error :: String -> a displays the given string as an error message and terminates the program; the polymorphic result type ensures 
+-- that error can be used in any context.
